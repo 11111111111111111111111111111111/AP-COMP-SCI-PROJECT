9c8abd721490ee1720f7eda53951cbe220234340
Aditya <aditya.nese@yahoo.com>
2015-05-13 15:07:22 -0500

    hi

---
+++ proj/CirclePanel.java
@@ -0,0 +1,149 @@
+// ******************************************************************
+//   CirclePanel.java
+//
+//   A panel with a circle drawn in the center and buttons on the 
+//   bottom that move the circle.
+// ******************************************************************
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+public class CirclePanel extends JPanel
+{
+    private final int CIRCLE_SIZE = 50;
+    private int x,y;
+    private Color c;
+    private JButton choose;
+    //---------------------------------------------------------------
+    // Set up circle and buttons to move it.
+    //---------------------------------------------------------------
+    public CirclePanel(int width, int height)
+    {
+    // Set coordinates so circle starts in middle
+    x = (width/2)-(CIRCLE_SIZE/2);
+    y = (height/2)-(CIRCLE_SIZE/2); 
+
+    c = Color.white;
+    
+    // Need a border layout to get the buttons on the bottom
+    this.setLayout(new BorderLayout());
+    
+    // Create buttons to move the circle
+    JButton left = new JButton("Left");
+    JButton right = new JButton("Right");
+    JButton up = new JButton("Up");
+    JButton down = new JButton("Down");
+
+    // Add listeners to the buttons
+    left.addActionListener(new MoveListener(-20,0));
+    right.addActionListener(new MoveListener(20,0));
+    up.addActionListener(new MoveListener(0,-20));
+    down.addActionListener(new MoveListener(0,20));
+
+    
+    
+    
+    
+    // Need a panel to put the buttons on or they'll be on
+    // top of each other.
+    JPanel buttonPanel = new JPanel();
+    buttonPanel.add(left);
+    buttonPanel.add(right);
+    buttonPanel.add(up);
+    buttonPanel.add(down);
+
+    // Add the button panel to the bottom of the main panel
+    this.add(buttonPanel, BorderLayout.SOUTH);
+    
+    JPanel colorPanel=new JPanel();
+    
+    JButton red = new JButton("Red");
+    red.setForeground(Color.red);
+    JButton blue = new JButton("Blue");
+    blue.setForeground(Color.blue);
+    JButton green = new JButton("Green");
+    green.setForeground(Color.green);
+    JButton yellow = new JButton("Yellow");
+    yellow.setForeground(Color.yellow);
+    
+    choose=new JButton("Choose Color");
+    
+    red.addActionListener(new ColorListener(Color.red));
+    blue.addActionListener(new ColorListener(Color.blue));
+    choose.addActionListener(new ColorListener(null));
+    green.addActionListener(new ColorListener(Color.green));
+    yellow.addActionListener(new ColorListener(Color.yellow));
+    
+    
+    
+    colorPanel.add(red);
+    colorPanel.add(blue);
+    colorPanel.add(choose);
+    colorPanel.add(green);
+    colorPanel.add(yellow);
+    this.add(colorPanel, BorderLayout.NORTH);
+    }
+
+    //---------------------------------------------------------------
+    // Draw circle on CirclePanel
+    //---------------------------------------------------------------
+    public void paintComponent(Graphics page)
+    {
+    super.paintComponent(page);
+
+    page.setColor(c);
+    page.fillOval(x,y,CIRCLE_SIZE,CIRCLE_SIZE);
+    }
+
+    //---------------------------------------------------------------
+    // Class to listen for button clicks that move circle.
+    //---------------------------------------------------------------
+    private class MoveListener implements ActionListener
+    {
+    private int dx;
+    private int dy;
+
+    //---------------------------------------------------------------
+    // Parameters tell how to move circle at click.
+    //---------------------------------------------------------------
+    public MoveListener(int dx, int dy)
+    {
+        this.dx = dx;
+        this.dy = dy;
+    }
+
+    //---------------------------------------------------------------
+    // Change x and y coordinates and repaint.
+    //---------------------------------------------------------------
+    public void actionPerformed(ActionEvent e)
+    {
+        x += dx;
+        y += dy;
+        repaint();
+    }
+    }
+    
+    private class ColorListener implements ActionListener
+    {
+        private Color a;
+        private JColorChooser g;
+        public ColorListener(Color a){
+            this.a=a;
+            g=new JColorChooser();
+        }
+        
+        public void actionPerformed(ActionEvent e)
+        {
+            if (a==null){
+                c=g.showDialog(choose, "Colors", c);
+            }
+            else
+            c=a;
+            repaint();
+            
+        }
+        
+        
+        
+        
+    }
+}
--- proj/GUI.java
+++ proj/GUI.java
@@ -1,117 +1,27 @@
+// ******************************************************************
+//   MoveCircle.java
+//
+//   Uses CirclePanel to display a GUI that lets the user move
+//   a circle by pressing buttons.
+// ******************************************************************
 
 import java.awt.*;
 import javax.swing.*;
 
-/**
- * Class GUI - write a description of the class here
- * 
- * @author (your name) 
- * @version (a version number)
- */
-public class GUI extends JApplet
+public class GUI
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Called by the browser or applet viewer to inform this JApplet that it
-     * has been loaded into the system. It is always called before the first 
-     * time that the start method is called.
-     */
-    public void init()
+    //-----------------------------------
+    //  Set up a frame for the GUI.
+    //-----------------------------------
+    public static void main(String[] args)
     {
-        // this is a workaround for a security conflict with some browsers
-        // including some versions of Netscape & Internet Explorer which do 
-        // not allow access to the AWT system event queue which JApplets do 
-        // on startup to check access. May not be necessary with your browser. 
-        JRootPane rootPane = this.getRootPane();    
-        rootPane.putClientProperty("defeatSystemEventQueueCheck", Boolean.TRUE);
-    
-        // provide any initialisation necessary for your JApplet
-    }
+	JFrame frame = new JFrame ("MoveCircle");
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.setPreferredSize(new Dimension(500,500));
 
-    /**
-     * Called by the browser or applet viewer to inform this JApplet that it 
-     * should start its execution. It is called after the init method and 
-     * each time the JApplet is revisited in a Web page. 
-     */
-    public void start()
-    {
-        // provide any code requred to run each time 
-        // web page is visited
-    }
+	frame.getContentPane().add(new CirclePanel(500,500));
 
-    /** 
-     * Called by the browser or applet viewer to inform this JApplet that
-     * it should stop its execution. It is called when the Web page that
-     * contains this JApplet has been replaced by another page, and also
-     * just before the JApplet is to be destroyed. 
-     */
-    public void stop()
-    {
-        // provide any code that needs to be run when page
-        // is replaced by another page or before JApplet is destroyed 
-    }
-
-    /**
-     * Paint method for applet.
-     * 
-     * @param  g   the Graphics object for this applet
-     */
-    public void paint(Graphics g)
-    {
-        // simple text displayed on applet
-        g.setColor(Color.white);
-        g.fillRect(0, 0, 200, 100);
-        g.setColor(Color.black);
-        g.drawString("Sample Applet", 20, 20);
-        g.setColor(Color.blue);
-        g.drawString("created by BlueJ", 20, 40);
-    }
-
-    /**
-     * Called by the browser or applet viewer to inform this JApplet that it
-     * is being reclaimed and that it should destroy any resources that it
-     * has allocated. The stop method will always be called before destroy. 
-     */
-    public void destroy()
-    {
-        // provide code to be run when JApplet is about to be destroyed.
-    }
-
-
-    /**
-     * Returns information about this applet. 
-     * An applet should override this method to return a String containing 
-     * information about the author, version, and copyright of the JApplet.
-     *
-     * @return a String representation of information about this JApplet
-     */
-    public String getAppletInfo()
-    {
-        // provide information about the applet
-        return "Title:   \nAuthor:   \nA simple applet example description. ";
-    }
-
-
-    /**
-     * Returns parameter information about this JApplet. 
-     * Returns information about the parameters than are understood by this JApplet.
-     * An applet should override this method to return an array of Strings 
-     * describing these parameters. 
-     * Each element of the array should be a set of three Strings containing 
-     * the name, the type, and a description.
-     *
-     * @return a String[] representation of parameter information about this JApplet
-     */
-    public String[][] getParameterInfo()
-    {
-        // provide parameter information about the applet
-        String paramInfo[][] = {
-                 {"firstParameter",    "1-10",    "description of first parameter"},
-                 {"status", "boolean", "description of second parameter"},
-                 {"images",   "url",     "description of third parameter"}
-        };
-        return paramInfo;
+	frame.pack();
+	frame.setVisible(true);
     }
 }
