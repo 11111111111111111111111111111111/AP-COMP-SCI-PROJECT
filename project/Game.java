efd72b05cd27446d42b9b3ff9740140665c584cb
Aditya <aditya.nese@yahoo.com>
2015-05-13 14:59:08 -0500

    added pic

---
+++ project/Game.java
@@ -0,0 +1,15 @@
+
+/**
+ * Abstract class game - write a description of the class here
+ * 
+ * @author (your name here)
+ * @version (version number or date here)
+ */
+public abstract class Game
+{
+    // instance variables - replace the example below with your own
+    
+}
+/*
+ * 
+ */
