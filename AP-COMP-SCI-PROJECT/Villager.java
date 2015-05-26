f13ef323c2b0c9b73bc1d644438b1691384987b4
PrahladSrikant <prahladsrikant@yahoo.com>
2015-05-26 09:19:38 -0500

    Revert "deleted unnecessary files"
    This reverts commit 9740317a39f80dbdbcd8b4f5c63a3d86311f95f9.

---
+++ AP-COMP-SCI-PROJECT/Villager.java
@@ -0,0 +1,34 @@
+
+/**
+ * Write a description of class Villager here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Villager
+{
+    // instance variables - replace the example below with your own
+    private int population;
+
+    /**
+     * Constructor for objects of class Villager
+     */
+    public Villager()
+    {
+        
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public void endOfTheDay()
+    {
+        // put your code here
+        return x + y;
+    }
+    
+    
+}
---
+++ AP-COMP-SCI-PROJECT/barracks.java
@@ -0,0 +1,31 @@
+
+/**
+ * Write a description of class barracks here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class barracks
+{
+    // instance variables - replace the example below with your own
+    private int max;
+    private int population;
+
+    /**
+     * Constructor for objects of class barracks
+     */
+    public barracks(int num)
+    {
+        
+        population=0;
+        max=num*5;
+        
+        
+    }
+
+    
+    public void add()
+    {
+        // put your code her
+    }
+}
